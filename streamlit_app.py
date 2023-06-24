835349d0a2ec97cfbdf20f477b421d9d9851c098
tyler.richards <tyler.richards@snowflake.com>
2023-06-24 09:30:02 -0700

    added elo ranking system

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,27 @@ df = pd.DataFrame({'Sandwich One': sandwiches, 'Sandwich Two': sandwiches_2, 'Sa
 
 st.subheader('Round One')
 edited_df = st.data_editor(df)
+
+#using elo sports
+from elosports.elo import Elo
+
+eloLeague = Elo(k = 20)
+eloLeague.addPlayer('BLT')
+eloLeague.addPlayer('Grilled Cheese')
+eloLeague.addPlayer('Peanut Butter & Jelly')
+eloLeague.addPlayer('Tuna Salad')
+
+#function to update elo scores
+def update_elo(df):
+    for i in range(len(df)):
+        if df['Sandwich One Victory?'][i] == True:
+            eloLeague.gameOver(winner=df['Sandwich One'][i], loser=df['Sandwich Two'][i], winnerHome=True)
+        else:
+            eloLeague.gameOver(winner=df['Sandwich Two'][i], loser=df['Sandwich One'][i], winnerHome=True)
+
+#update elo scores
+update_elo(edited_df)
+st.write(eloLeague.ratingDict)
+st.stop()
+elo_scores = update_elo(edited_df)
+st.write(elo_scores)
